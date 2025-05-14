8ed2ffdd8a6425de5991752c79236c2995acad02
leetk77777 <leetk2495@naver.com>
2025-05-14 14:31:05 +0900

    aop 어노테이션 활용 및 로그 정확히 보이도록 수정

--- BoardApplication/src/main/java/com/example/board/aop/LoggingAspect.java
+++ BoardApplication/src/main/java/com/example/board/aop/LoggingAspect.java
@@ -11,11 +11,12 @@ public class LoggingAspect {
 
 	@Around("@annotation(com.example.board.aop.LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		System.out.println("AOP 실행 시작: " + joinPoint.getSignature());
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed(); // 실제 메서드 실행
         long executionTime = System.currentTimeMillis() - start;
 
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        System.out.println(joinPoint.getSignature() + " 실행시간 : " + executionTime + "ms");
         return proceed;
     }
 }
